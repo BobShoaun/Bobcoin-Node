e0100446d6b0dd15d2d29dc28ecff2063d4bff58
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-06-02 11:26:24 +0800

    change port var

--- src/settings.js
+++ src/settings.js
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const port = process.env.port || 3001;
+export const port = process.env.PORT || 3001;
 export const atlasURI = process.env.ATLAS_URI;
