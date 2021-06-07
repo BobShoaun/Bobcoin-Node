957d0f14084f7880b2cb45037c2d06317f7c72b4
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-06-07 10:31:57 +0800

    clear git cache

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const port = process.env.PORT || 3001;
-export const atlasURI = process.env.ATLAS_URI;
+export const atlasURI = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.owydh.mongodb.net/Bobcoin-Node?retryWrites=true&w=majority`;
